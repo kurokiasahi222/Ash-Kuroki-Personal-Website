922a9132cba1cc71d4cc58b016167539f681b8d0
Asahi Kuroki <kurok011@umn.edu>
2023-09-14 13:09:07 -0500

    fixed fullpage.js error

---
+++ src/layouts/script.js
@@ -0,0 +1,9 @@
+// new fullpage('#fullpage', {
+//     //options here
+//     licenseKey: 'L3B16-Q1HWH-KKFRH-9MJ38-RXWYM',
+//     autoScrolling: true,
+//     sectionsColor: ['#f9f9f9', 'white', '#f9f9f9'],
+//     // fitToSection: false,
+//     // fixedElements: '#header', 
+//     // paddingTop: '4rem',
+// }); 
