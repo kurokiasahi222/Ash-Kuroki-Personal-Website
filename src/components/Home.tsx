aba24731960f55e5ac931b8960ef567782ada3f5
Asahi Kuroki <kurok011@umn.edu>
2024-05-31 21:08:13 -0500

    wip

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -7,12 +7,13 @@ export default function Home() {
     const el = React.useRef(null);
     React.useEffect(() => {
         const typed = new Typed(el.current, {
-            // strings: ['Software Developer', 'AI Enginner', 'Aspiring Entreprenur'],
-            strings: ['Software Developer'], 
+            strings: ['Software Developer', 'AI Enginner', 'Aspiring Entreprenur'],
+            // strings: ['Software Developer'], 
             typeSpeed: 80,
             backSpeed: 10,
             startDelay: 1000,
             loop: false,
+            showCursor: false,
         });
         return () => {
             // Destroy Typed instance during cleanup to stop animation
@@ -21,7 +22,7 @@ export default function Home() {
     }, []);
 
     return (
-        <div>
+        <>
             <div className={styles.aboutMe}>
                 <div className={styles.content}>
                     <div className={styles.intro}>
@@ -30,7 +31,7 @@ export default function Home() {
                     </div>
                     <span className={styles.title} ref={el} />
                     <p className={styles.description}>
-                        I'm a <b>Full Stack Developer </b>specializing in building (and occasionally designing) exceptional digital experiences.
+                        I'm a <b>Full Stack Developer </b>based in Minneapolis.I love working in all aspect of tech stack from frontend to backend to AI. 
                         <br />
                         This website is created using <i><a className={styles.link} href="https://astro.build/">Astro.js </a></i> & <i><a className={styles.link} href="https://alvarotrigo.com/fullPage/">full-page.js</a></i>
                     </p>
@@ -38,10 +39,20 @@ export default function Home() {
                         <h2 className={styles.project}><a href="#projects">PROJECTS</a></h2>
                     </div>
                 </div>
-                <div className={styles.imageContainer}>
-                    <img className={styles.ashImg}src="../../images/asahi-headshot.png" alt="asahi-headshot" />
+                <div className={styles.imageContainer} >
+                    <div className={styles.flipper}>
+                        <div className={styles.front}>
+                            <img className={styles.ashImg} src="../../images/asahi-headshot.png" alt="asahi-headshot" />
+                        </div>
+                        <div className={styles.back}>
+                            <h1>Fun fact: I am from Japan and I have been studying abroad in the US since
+                                2016!
+                            </h1>
+                        </div>
+                    </div>
+                    
                 </div>
             </div>
-        </div>
+        </>
     );
 }
