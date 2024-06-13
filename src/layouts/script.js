3b540a29debc76cae276ceb1fd04e46b16a55739
Asahi Kuroki <kurok011@umn.edu>
2024-06-13 17:50:25 -0500

    created hambergermenu transition animation

--- src/layouts/script.js
+++ src/layouts/script.js
@@ -1,12 +1,18 @@
 const getKey = () => {
     return 'L3B16-Q1HWH-KKFRH-9MJ38-RXWYM';
 }
-// new fullpage('#fullpage', {
-//     //options here
-//     licenseKey: 'L3B16-Q1HWH-KKFRH-9MJ38-RXWYM',
-//     autoScrolling: true,
-//     sectionsColor: ['#f9f9f9', 'white', '#f9f9f9'],
-//     // fitToSection: false,
-//     // fixedElements: '#header', 
-//     // paddingTop: '4rem',
-// }); 
+new fullpage("#fullpage", {
+    licenseKey: 'L3B16-Q1HWH-KKFRH-9MJ38-RXWYM',
+    anchors: ["section1", "section2", "section3", "section4"],
+    // sectionsColor: ["#f9f9f9"],
+    sectionsColor: ["#f9f9f9", "#E6F3F4", "#f9f9f9", "#f9f9f9"],
+    fixedElements: "#header",
+    paddingTop: "4.375rem",
+    scrollOverflow: true,
+    scrollingSpeed: 1000,
+    navigation: true,
+    showActiveTooltip: true,
+    credits: {
+        enabled: false,
+    },
+});
