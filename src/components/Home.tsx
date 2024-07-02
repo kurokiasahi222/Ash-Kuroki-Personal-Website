8494809d5576982a578f42240f58f680a80f2dbe
Asahi Kuroki <kurok011@umn.edu>
2024-07-01 20:40:38 -0500

    css

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Typed from 'typed.js';
-import styles from '../styles/home.module.css';
+import styles from '../styles/home.module.scss';
 
 export default function Home() {
     // Create reference to store the DOM element containing the animation
@@ -25,7 +25,7 @@ export default function Home() {
         <div>
             <div className={styles.container}>
                 <div className={styles.aboutMe}>
-                    {/* <div className={styles.centerLine}></div> */}
+                    <div className={styles.centerLine}></div>
                     <div className={styles.content}>
                         <div className={styles.intro}>
                             <h1>Hi! I'm </h1>
@@ -41,16 +41,7 @@ export default function Home() {
                         </p>
                     </div>
                     <div className={styles.imageContainer} >
-                        <div className={styles.flipper}>
-                            <div className={styles.front}>
-                                <img className={styles.ashImg} src="../../images/asahi-headshot.webp" alt="asahi-headshot" />
-                            </div>
-                            <div className={styles.back}>
-                                <h1>I was born and raised in Japan, and I have been studying abroad in the US since
-                                    2016!
-                                </h1>
-                            </div>
-                        </div>
+                        <img className={styles.ashImg} src="../../images/asahi-headshot.webp" alt="asahi-headshot" />
                     </div>
                 </div>
             </div>
