9ca2a80cf906902a2cd348d8dfac0975b66d7691
Asahi Kuroki <kurok011@umn.edu>
2024-07-13 15:15:13 -0500

    wip

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -66,6 +66,7 @@ export default function Home() {
           </div>
         </div>
       </div>
+      <div className={styles.wave}></div>
     </div>
   );
 }
--- src/components/animation.js
+++ src/components/animation.js
@@ -1,16 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
-
     const hiddenElems = document.querySelectorAll(".animation");
-    const observer = new IntersectionObserver((entries) => {
+    const options = {
+        threshold: 0.4,
+    }
+    const observerCallback = (entries) => {
         entries.forEach((entry) => {
             console.log(entry);
             if (entry.isIntersecting) {
                 entry.target.classList.add("intersect");
             }
             else {
-            entry.target.classList.remove("intersect");
+                entry.target.classList.remove("intersect");
             }
         });
-    });
+    };
+    const observer = new IntersectionObserver(observerCallback, options);
     hiddenElems.forEach((el) => observer.observe(el));
 })
