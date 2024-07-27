3358d610cadb1eeb9920722f96f507bf08230940
Asahi Kuroki <kurok011@umn.edu>
2024-07-26 22:40:59 -0500

    wip

--- src/components/animation.js
+++ src/components/animation.js
@@ -16,3 +16,23 @@ document.addEventListener("DOMContentLoaded", () => {
   const observer = new IntersectionObserver(observerCallback, options);
   hiddenElems.forEach((el) => observer.observe(el));
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const hiddenElems = document.querySelectorAll(".image.left");
+  const options = {
+    threshold: 0.2,
+  };
+  const observerCallback = (entries) => {
+    entries.forEach((entry) => {
+      console.log("Image intersection");
+      if (entry.isIntersecting) {
+        entry.target.classList.add("intersect");
+      } else {
+        entry.target.classList.remove("intersect");
+      }
+    });
+  };
+  const observer = new IntersectionObserver(observerCallback, options);
+  hiddenElems.forEach((el) => observer.observe(el));
+});
+
